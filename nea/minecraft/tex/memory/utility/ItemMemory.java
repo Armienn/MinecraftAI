2b0787b89fc939618d19809cb9571b6500cb98fa
Armienn <nigathr@gmail.com>
2015-02-27 14:19:26 +0100

    Finished position analysis of items

--- nea/minecraft/tex/memory/utility/ItemMemory.java
+++ nea/minecraft/tex/memory/utility/ItemMemory.java
@@ -27,6 +27,55 @@ public class ItemMemory {
 	}
 	
 	public void Update(ItemInfo item, long time){
-		// TODO
+		double deltaX = item.posX - CurrentPosX();
+		double deltaY = item.posY - CurrentPosY();
+		double deltaZ = item.posZ - CurrentPosZ();
+		
+		double delta = Math.abs(deltaX) + Math.abs(deltaY) + Math.abs(deltaZ);
+		if(delta > 0.0001){
+			if(movements.size() > 0){
+				Movement prevmove = movements.get(movements.size()-1);
+				if( prevmove.endTime < lastUpdate ){ // if last movement ended before last update
+					movements.add(new Movement(lastUpdate,CurrentPosX(),CurrentPosY(),CurrentPosZ(),time,item.posX,item.posY,item.posZ));
+				}
+				else { // if last movement was still ongoing last update
+					prevmove.endTime = time;
+					prevmove.endPosX = item.posX;
+					prevmove.endPosY = item.posY;
+					prevmove.endPosZ = item.posZ;
+				}
+			}
+			else {
+				movements.add(new Movement(lastUpdate,CurrentPosX(),CurrentPosY(),CurrentPosZ(),time,item.posX,item.posY,item.posZ));
+			}
+		}
+		lastUpdate = time;
+	}
+	
+	public double CurrentPosX(){
+		if(movements.size() > 0){
+			return movements.get(movements.size()-1).endPosX;
+		}
+		else{
+			return appearPosX;
+		}
+	}
+	
+	public double CurrentPosY(){
+		if(movements.size() > 0){
+			return movements.get(movements.size()-1).endPosY;
+		}
+		else{
+			return appearPosY;
+		}
+	}
+	
+	public double CurrentPosZ(){
+		if(movements.size() > 0){
+			return movements.get(movements.size()-1).endPosZ;
+		}
+		else{
+			return appearPosZ;
+		}
 	}
 }
--- nea/minecraft/tex/memory/utility/Movement.java
+++ nea/minecraft/tex/memory/utility/Movement.java
@@ -1,13 +1,24 @@
 package nea.minecraft.tex.memory.utility;
 
 public class Movement {
-	public long starttime;
-	public double startposX;
-	public double startposY;
-	public double startposZ;
+	public long startTime;
+	public double startPosX;
+	public double startPosY;
+	public double startPosZ;
 
-	public long endtime;
-	public double endposX;
-	public double endposY;
-	public double endposZ;
+	public long endTime;
+	public double endPosX;
+	public double endPosY;
+	public double endPosZ;
+	
+	public Movement(long starttime, double sx, double sy, double sz, long endtime, double ex, double ey, double ez){
+		startTime = starttime;
+		startPosX = sx;
+		startPosY = sy;
+		startPosZ = sz;
+		endTime = endtime;
+		endPosX = ex;
+		endPosY = ey;
+		endPosZ = ez;
+	}
 }
